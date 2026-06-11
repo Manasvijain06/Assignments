509673ab3e8a8c5e3f7ab8592ffbc2d71c684e1a
manasvijain06 <manasvijain61104@gmail.com>
2026-06-11 20:37:30 +0530

    feat: create python training project structure

---
+++ manasvijain_python_training/python_basic/Introduction/q1.py
@@ -0,0 +1,9 @@
+""" Question 1 : Write a program to print 'Welcome to Python Training'."""
+
+def print_welcome_message() -> None:
+    """Display welcome message."""
+    print("Welcome to Python Training")
+
+
+if __name__ == "__main__":
+    print_welcome_message()
