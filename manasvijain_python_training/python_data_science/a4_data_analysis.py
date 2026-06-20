058e9bd08b65a85a747584fa05057cbc78767c70
manasvijain06 <manasvijain61104@gmail.com>
2026-06-20 13:47:11 +0530

    feat: Implemented avg, max,count function on employee dataset.

--- manasvijain_python_training/python_data_science/a4_data_analysis.py
+++ manasvijain_python_training/python_data_science/a4_data_analysis.py
@@ -4,14 +4,14 @@ from a2_pandas_dataframe import get_employee_data
 # get employees DataFrame
 df = get_employee_data()
 
-# Average salary by department
+# 1. Find average salary by department
 print("Average Salary by Department:")
 print(df.groupby("Department")["Salary"].mean())
 
-# Maximum salary by department
+# 2.Find maximum salary by department
 print("\nMaximum Salary by Department:")
 print(df.groupby("Department")["Salary"].max())
 
-# Count employees per department
+# 3.Count employees per department
 print("\nEmployee Count by Department:")
 print(df.groupby("Department")["Name"].count())
